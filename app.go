e32a7ef7acd4d6b6ea955e0b9119952e4ca2983d
rrguhan6 <rrguhan6@gmail.com>
2021-02-13 19:19:46 +0530

    -

--- app.go
+++ app.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,40 +17,16 @@ func errorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
 }
 
-func convertToNestedData(d DATA) NestedData {
-	return NestedData{
-		Event:           d.Ev,
-		EventType:       d.Et,
-		AppID:           d.ID,
-		UserID:          d.UID,
-		MessageID:       d.Mid,
-		PageTitle:       d.T,
-		PageURL:         d.P,
-		BrowserLanguage: d.L,
-		ScreenSize:      d.Sc,
-		Attributes: Attributes{
-			FormVarient: ValueType{d.Atrv1, d.Atrt1},
-			Ref:         ValueType{d.Atrv2, d.Atrt2},
-		},
-		Traits: Traits{
-			Name:  ValueType{d.Uatrv1, d.Uatrt1},
-			Email: ValueType{d.Uatrv2, d.Uatrt2},
-			Age:   ValueType{d.Uatrv3, d.Uatrt3},
-		},
-	}
-}
-
 var e error
 
-func sendToWebhook(d DATA, c chan int) {
+func sendToWebhook(d map[string]string, c chan int) {
 
-	_d := convertToNestedData(d)
-	jsonValue, e := json.Marshal(conventionalMarshaller{_d})
+	_d := converter(d)
 	if e != nil {
 		c <- 500
 		return
 	}
-	resp, e := http.Post("https://webhook.site/7dce8115-29d8-4f04-bba7-1e5f8850c66a", "application/json", bytes.NewBuffer(jsonValue))
+	resp, e := http.Post("https://webhook.site/7bca21aa-8fe9-4b1e-87a2-74aa653a12c3", "application/json", bytes.NewBuffer(_d))
 	if e != nil {
 		c <- 500
 		return
@@ -60,8 +35,18 @@ func sendToWebhook(d DATA, c chan int) {
 
 }
 
+func worker(jobs <-chan map[string]string, results chan<- int) {
+	for j := range jobs {
+		ch := make(chan int)
+		go sendToWebhook(j, ch)
+		_status := <-ch
+		results <- _status
+	}
+}
+
 func form(c *gin.Context) {
-	var d DATA
+	d := map[string]string{}
+
 	err := c.ShouldBindJSON(&d)
 
 	if err != nil {
@@ -69,19 +54,26 @@ func form(c *gin.Context) {
 		return
 	}
 
-	ch := make(chan int)
-	go sendToWebhook(d, ch)
+	jobs <- d
 
-	_status := <-ch
+	_status := <-results
 
 	c.JSON(_status, gin.H{"status": _status})
 }
 
+var jobs = make(chan map[string]string, 100)
+var results = make(chan int, 100)
+
 func main() {
 	r := gin.Default()
+
+	for i := 0; i < 100; i++ {
+		go worker(jobs, results)
+	}
 
 	r.GET("/ping", ping)
 	r.POST("/form", form)
 
 	r.Run(":8000")
+
 }
--- utils.go
+++ utils.go
@@ -1,87 +1,72 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"regexp"
 )
-
-var keyMatchRegex = regexp.MustCompile(`\"(\w+)\":`)
-var wordBarrierRegex = regexp.MustCompile(`(\w)([A-Z]{1,})`)
-
-type conventionalMarshaller struct {
-	Value interface{}
-}
-
-func (c conventionalMarshaller) MarshalJSON() ([]byte, error) {
-	marshalled, err := json.Marshal(c.Value)
-
-	converted := keyMatchRegex.ReplaceAllFunc(
-		marshalled,
-		func(match []byte) []byte {
-			return bytes.ToLower(wordBarrierRegex.ReplaceAll(
-				match,
-				[]byte(`${1}_${2}`),
-			))
-		},
-	)
-
-	return converted, err
-}
-
-type DATA struct {
-	Ev     string `json:"ev" binding:"required"`
-	Et     string `json:"et" binding:"required"`
-	ID     string `json:"id" binding:"required"`
-	UID    string `json:"uid" binding:"required"`
-	Mid    string `json:"mid" binding:"required"`
-	T      string `json:"t" binding:"required"`
-	P      string `json:"p" binding:"required"`
-	L      string `json:"l" binding:"required"`
-	Sc     string `json:"sc" binding:"required"`
-	Atrk1  string `json:"atrk1" binding:"required"`
-	Atrv1  string `json:"atrv1" binding:"required"`
-	Atrt1  string `json:"atrt1" binding:"required"`
-	Atrk2  string `json:"atrk2" binding:"required"`
-	Atrv2  string `json:"atrv2" binding:"required"`
-	Atrt2  string `json:"atrt2" binding:"required"`
-	Uatrk1 string `json:"uatrk1" binding:"required"`
-	Uatrv1 string `json:"uatrv1" binding:"required"`
-	Uatrt1 string `json:"uatrt1" binding:"required"`
-	Uatrk2 string `json:"uatrk2" binding:"required"`
-	Uatrv2 string `json:"uatrv2" binding:"required"`
-	Uatrt2 string `json:"uatrt2" binding:"required"`
-	Uatrk3 string `json:"uatrk3" binding:"required"`
-	Uatrv3 string `json:"uatrv3" binding:"required"`
-	Uatrt3 string `json:"uatrt3" binding:"required"`
-}
 
 type ValueType struct {
 	Value string
 	Type  string
 }
 
-type Attributes struct {
-	FormVarient ValueType
-	Ref         ValueType
-}
+var md = make(map[string]string)
 
-type Traits struct {
-	Name  ValueType
-	Email ValueType
-	Age   ValueType
-}
+func converter(d map[string]string) []byte {
 
-type NestedData struct {
-	Event           string
-	EventType       string
-	AppID           string
-	UserID          string
-	MessageID       string
-	PageTitle       string
-	PageURL         string
-	BrowserLanguage string
-	ScreenSize      string
-	Attributes      Attributes
-	Traits          Traits
+	md["ev"] = "event"
+	md["et"] = "event_type"
+	md["id"] = "app_id"
+	md["uid"] = "user_id"
+	md["mid"] = "message_id"
+	md["t"] = "page_title"
+	md["p"] = "page_url"
+	md["l"] = "browser_language"
+	md["sc"] = "screen_size"
+
+	keys := make([]string, 0, len(d))
+
+	for k := range d {
+		keys = append(keys, k)
+	}
+	result := make(map[string]interface{})
+
+	attr := map[string]interface{}{}
+	trai := map[string]interface{}{}
+
+	for k := range keys {
+
+		if len(keys[k]) > 4 {
+			l := keys[k][:4]
+			if l == "atrk" {
+
+				key := keys[k]
+				val := "atrv" + keys[k][4:]
+				ty := "atrt" + keys[k][4:]
+
+				attr[d[key]] = ValueType{d[val], d[ty]}
+
+			} else if l == "uatr" {
+				if keys[k][:5] == "uatrk" {
+					key := keys[k]
+					val := "uatrv" + keys[k][5:]
+					ty := "uatrt" + keys[k][5:]
+					trai[d[key]] = ValueType{d[val], d[ty]}
+				}
+			} else {
+				key := keys[k]
+
+				result[md[key]] = d[key]
+			}
+		} else {
+
+			key := keys[k]
+
+			result[md[key]] = d[key]
+		}
+	}
+	result["attributes"] = attr
+	result["traits"] = trai
+	json1, _ := json.Marshal(result)
+
+	return json1
 }
